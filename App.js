ed37b1c06aacb61fda4ee88385fccf7e905f0209
Vinicius Mendite <vinicius.miff@gmail.com>
2020-09-01 23:46:27 -0300

    criando estrutura do contextapi

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 
+import UserContextProvider from './src/contexts/UserContext';
 import MainStack from './src/routes/MainStack';
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <MainStack />
-    </NavigationContainer>
+    <UserContextProvider>
+      <NavigationContainer>
+        <MainStack />
+      </NavigationContainer>
+    </UserContextProvider>
   );
 };
 
--- src/pages/Preload/index.js
+++ src/pages/Preload/index.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import AsyncStorage from '@react-native-community/async-storage';
+import {useNavigation} from '@react-navigation/native';
 
 import {Container, LoadingIcon} from './styles';
 
 import BarberLogo from '../../assets/barber.svg';
 
 const Preload = () => {
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    const checkToken = async () => {
+      const token = await AsyncStorage.getItem('token');
+      if (token) {
+      } else {
+        navigation.navigate('SignIn');
+      }
+    };
+    checkToken();
+  }, []);
   return (
     <Container>
       <BarberLogo width="100%" height="160" />
