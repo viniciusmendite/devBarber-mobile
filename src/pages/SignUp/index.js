7d3490c3610fc7bc541c42f90089c71b77161f81
Vinicius Mendite <vinicius.miff@gmail.com>
2020-09-02 23:11:26 -0300

    criando tela cadastro

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,9 +1,76 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {StatusBar} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
 
-import {Container} from './styles';
+import {
+  Container,
+  InputArea,
+  CustomButton,
+  CustomButtonText,
+  SignMessageButton,
+  SignMessageButtonText,
+  SignMessageButtonTextBold,
+} from './styles';
+
+import BarberLogo from '../../assets/barber.svg';
+import PersonIcon from '../../assets/person.svg';
+import EmailIcon from '../../assets/email.svg';
+import LockIcon from '../../assets/lock.svg';
+
+import SignInput from '../../components/SignInput';
 
 const SignUp = () => {
-  return <Container />;
+  const navigation = useNavigation();
+
+  const [nameField, setNameField] = useState('');
+  const [emailField, setEmailField] = useState('');
+  const [passwordField, setPasswordField] = useState('');
+
+  const handleMessageButtonClick = () => {
+    navigation.reset({
+      routes: [{name: 'SignIn'}],
+    });
+  };
+
+  const handleSignClick = () => {};
+
+  return (
+    <Container>
+      <StatusBar backgroundColor="#63c2d1" barStyle="light-content" />
+      <BarberLogo width="100%" height="160" />
+
+      <InputArea>
+        <SignInput
+          IconSvg={PersonIcon}
+          placeholder="Digite seu nome"
+          value={nameField}
+          onChangeText={(text) => setNameField(text)}
+        />
+        <SignInput
+          IconSvg={EmailIcon}
+          placeholder="Digite seu e-mail"
+          value={emailField}
+          onChangeText={(text) => setEmailField(text)}
+        />
+        <SignInput
+          IconSvg={LockIcon}
+          placeholder="Digite sua senha"
+          value={passwordField}
+          onChangeText={(text) => setPasswordField(text)}
+          password={true}
+        />
+
+        <CustomButton onPress={handleSignClick}>
+          <CustomButtonText>Cadastrar</CustomButtonText>
+        </CustomButton>
+      </InputArea>
+
+      <SignMessageButton onPress={handleMessageButtonClick}>
+        <SignMessageButtonText>Já possui uma conta?</SignMessageButtonText>
+        <SignMessageButtonTextBold>Faça Login</SignMessageButtonTextBold>
+      </SignMessageButton>
+    </Container>
+  );
 };
 
 export default SignUp;
