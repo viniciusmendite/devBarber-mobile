b57b0f3c6ff833f5a94e8299ee6621c5ad72a47e
Vinicius Mendite <vinicius.miff@gmail.com>
2020-09-02 22:39:48 -0300

    criando layout da tela login

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,9 +1,43 @@
 import React from 'react';
 
-import {Container} from './styles';
+import {
+  Container,
+  InputArea,
+  CustomButton,
+  CustomButtonText,
+  SignMessageButton,
+  SignMessageButtonText,
+  SignMessageButtonTextBold,
+} from './styles';
+
+import BarberLogo from '../../assets/barber.svg';
+import EmailIcon from '../../assets/email.svg';
+import LockIcon from '../../assets/lock.svg';
+
+import SignInput from '../../components/SignInput';
 
 const SignIn = () => {
-  return <Container />;
+  return (
+    <Container>
+      <BarberLogo width="100%" height="160" />
+
+      <InputArea>
+        <SignInput IconSvg={EmailIcon} placeholder="Digite seu e-mail" />
+        <SignInput IconSvg={LockIcon} placeholder="Digite sua senha" />
+
+        <CustomButton>
+          <CustomButtonText>Login</CustomButtonText>
+        </CustomButton>
+      </InputArea>
+
+      <SignMessageButton>
+        <SignMessageButtonText>
+          Ainda não possui uma conta?
+        </SignMessageButtonText>
+        <SignMessageButtonTextBold>Cadastre-se</SignMessageButtonTextBold>
+      </SignMessageButton>
+    </Container>
+  );
 };
 
 export default SignIn;
