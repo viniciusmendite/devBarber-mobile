afa96f586489728a8c19b1be5cc2c2fd8e17b438
Vinicius Mendite <vinicius.miff@gmail.com>
2020-09-03 23:10:23 -0300

    importando bottomtabs na mainstack

--- src/routes/MainStack.js
+++ src/routes/MainStack.js
@@ -4,6 +4,7 @@ import {createStackNavigator} from '@react-navigation/stack';
 import Preload from '../pages/Preload';
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
+import MainTab from './MainTab';
 
 const {Navigator, Screen} = createStackNavigator();
 
@@ -16,6 +17,7 @@ const MainStack = () => (
     <Screen name="Preload" component={Preload} />
     <Screen name="SignIn" component={SignIn} />
     <Screen name="SignUp" component={SignUp} />
+    <Screen name="MainTab" component={MainTab} />
   </Navigator>
 );
 
