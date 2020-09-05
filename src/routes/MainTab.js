c77c03818a30d6c0d2495e52677a8a5f3a3e740d
Vinicius Mendite <vinicius.miff@gmail.com>
2020-09-04 22:56:30 -0300

    usando tabBar

--- src/routes/MainTab.js
+++ src/routes/MainTab.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
+import CustomTabBar from '../components/CustomTabBar';
 import Home from '../pages/Home';
 import Search from '../pages/Search';
 import Appointments from '../pages/Appointments';
@@ -10,7 +11,7 @@ import Profile from '../pages/Profile';
 const {Navigator, Screen} = createBottomTabNavigator();
 
 const MainTab = () => (
-  <Navigator>
+  <Navigator tabBar={(props) => <CustomTabBar {...props} />}>
     <Screen name="Home" component={Home} />
     <Screen name="Search" component={Search} />
     <Screen name="Appointments" component={Appointments} />
