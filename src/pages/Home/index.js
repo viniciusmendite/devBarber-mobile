06f5cfc2e0ba69929f7787316b0beb0c75dcd698
Vinicius Mendite <vinicius.miff@gmail.com>
2020-09-05 23:13:18 -0300

    criando header da tela home
    - importando icone de localização
    - criando layout do header

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,53 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {StatusBar} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
 
-import {Container} from './styles';
+import {
+  Container,
+  Scroller,
+  HeaderArea,
+  HeaderTitle,
+  SearchButton,
+  LocationArea,
+  LocationInput,
+  LocationFinder,
+} from './styles';
+
+import SearchIcon from '../../assets/search.svg';
+import MyLocationIcon from '../../assets/my_location.svg';
 
 const Home = () => {
-  return <Container />;
+  const navigation = useNavigation();
+
+  const [locationText, setLocationText] = useState('');
+
+  return (
+    <Container>
+      <StatusBar backgroundColor="#63c2d1" barStyle="light-content" />
+      <Scroller>
+        <HeaderArea>
+          <HeaderTitle numberOfLines={2}>
+            Encontre o seu barbeiro favorito
+          </HeaderTitle>
+          <SearchButton onPress={() => navigation.navigate('Search')}>
+            <SearchIcon width="26" height="26" fill="#FFF" />
+          </SearchButton>
+        </HeaderArea>
+
+        <LocationArea>
+          <LocationInput
+            placeholder="Onde você está?"
+            placeholderTextColor="#FFF"
+            value={locationText}
+            onChangeText={(text) => setLocationText(text)}
+          />
+          <LocationFinder>
+            <MyLocationIcon width="24" height="24" fill="#FFF" />
+          </LocationFinder>
+        </LocationArea>
+      </Scroller>
+    </Container>
+  );
 };
 
 export default Home;
