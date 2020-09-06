afb855aeb866dfe5ee7c98fb0efefa94d0a22cf9
Vinicius Mendite <vinicius.miff@gmail.com>
2020-09-05 23:55:06 -0300

    pegando permissão do usuario e salvando os barbeiros
    - pegando localização atual
    - salvando a lista de barbeiros no array
    - exibindo localização

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,8 @@
-import React, {useState} from 'react';
-import {StatusBar} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {StatusBar, Platform} from 'react-native';
 import {useNavigation} from '@react-navigation/native';
+import {request, PERMISSIONS} from 'react-native-permissions';
+import GeoLocation from '@react-native-community/geolocation';
 
 import {
   Container,
@@ -11,7 +13,10 @@ import {
   LocationArea,
   LocationInput,
   LocationFinder,
+  LoadingIcon,
 } from './styles';
+
+import api from '../../services/api';
 
 import SearchIcon from '../../assets/search.svg';
 import MyLocationIcon from '../../assets/my_location.svg';
@@ -20,6 +25,49 @@ const Home = () => {
   const navigation = useNavigation();
 
   const [locationText, setLocationText] = useState('');
+  const [coords, setCoords] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [list, setList] = useState([]);
+
+  const handleLocationFinder = async () => {
+    setCoords(null);
+    let result = await request(
+      Platform.OS === 'ios'
+        ? PERMISSIONS.IOS.LOCATION_WHEN_IN_USE
+        : PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION,
+    );
+
+    if (result === 'granted') {
+      setLoading(true);
+      setLocationText('');
+      setList([]);
+
+      GeoLocation.getCurrentPosition((info) => {
+        setCoords(info.coords);
+        getBarbers();
+      });
+    }
+  };
+
+  const getBarbers = async () => {
+    setLoading(true);
+    setList([]);
+
+    let response = await api.getBarbers();
+    if (response.error === '') {
+      if (response.loc) {
+        setLocationText(response.loc);
+      }
+      setList(response.data);
+    } else {
+      alert('Erro:' + response.error);
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getBarbers();
+  }, []);
 
   return (
     <Container>
@@ -41,10 +89,12 @@ const Home = () => {
             value={locationText}
             onChangeText={(text) => setLocationText(text)}
           />
-          <LocationFinder>
+          <LocationFinder onPress={handleLocationFinder}>
             <MyLocationIcon width="24" height="24" fill="#FFF" />
           </LocationFinder>
         </LocationArea>
+
+        {loading && <LoadingIcon size="large" color="#fff" />}
       </Scroller>
     </Container>
   );
