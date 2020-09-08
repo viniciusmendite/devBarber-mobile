bff6c2839048283b913197f1b77314fb0bd2425c
Vinicius Mendite <vinicius.miff@gmail.com>
2020-09-07 22:30:10 -0300

    buscando info de um único barbeiro

--- src/pages/Barber/index.js
+++ src/pages/Barber/index.js
@@ -1,8 +1,36 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {useNavigation, useRoute} from '@react-navigation/native';
 
 import {Container} from './styles';
 
+import api from '../../services/api';
+
 const Barber = () => {
+  const navigation = useNavigation();
+  const route = useRoute();
+
+  const [userInfo, setUserInfo] = useState({
+    id: route.params.id,
+    avatar: route.params.avatar,
+    name: route.params.name,
+    stars: route.params.stars,
+  });
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const getBarberInfo = async () => {
+      setLoading(true);
+      let json = await api.getBarber(userInfo.id);
+      if (json.error === '') {
+        setUserInfo(json.data);
+      } else {
+        alert('Erro: ' + json.error);
+      }
+      setLoading(false);
+    };
+    getBarberInfo();
+  }, []);
+
   return <Container />;
 };
 
